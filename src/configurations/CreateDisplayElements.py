6a040d317d4ecb0b4b761437181728b151a7a2e8
Gagan Sopori <9592538+gagansopori@users.noreply.github.com>
2022-08-17 18:49:59 -0500

    Update CreateDisplayElements.py

--- src/configurations/CreateDisplayElements.py
+++ src/configurations/CreateDisplayElements.py
@@ -1,32 +1,32 @@
-# import ST7789 as st7789
+import ST7789 as st7789
 import os
 from PIL import Image, ImageDraw, ImageFont
 
 
 class CreateDisplay:
-    # def __init__(self, *args, **kwargs):
-    #     # setting up the spi display on Pirate Audio Board
-    #     self.display = st7789.ST7789(port=0,
-    #                                  cs=st7789.BG_SPI_CS_FRONT,
-    #                                  dc=9,
-    #                                  rst=25,
-    #                                  backlight=13,
-    #                                  width=240,
-    #                                  height=240,
-    #                                  rotation=90,
-    #                                  spi_speed_hz=80 * 1000 * 100,
-    #                                  offset_left=0,
-    #                                  offset_top=0)
-    #     self.display.begin()
-    #     self.display.size = (self.display.width, self.display.height)
-    #
-    # def start(self):
-    #     self.display.command(st7789.ST7789_DISPON)
-    #     self.display.set_backlight(100)
-    #
-    # def stop(self):
-    #     self.display.command(st7789.ST7789_DISPOFF)
-    #     self.display.set_backlight(0)
+    def __init__(self, *args, **kwargs):
+        # setting up the spi display on Pirate Audio Board
+        self.display = st7789.ST7789(port=0,
+                                     cs=st7789.BG_SPI_CS_FRONT,
+                                     dc=9,
+                                     rst=25,
+                                     backlight=13,
+                                     width=240,
+                                     height=240,
+                                     rotation=90,
+                                     spi_speed_hz=80 * 1000 * 100,
+                                     offset_left=0,
+                                     offset_top=0)
+        self.display.begin()
+        self.display.size = (self.display.width, self.display.height)
+
+    def start(self):
+        self.display.command(st7789.ST7789_DISPON)
+        self.display.set_backlight(100)
+
+    def stop(self):
+        self.display.command(st7789.ST7789_DISPOFF)
+        self.display.set_backlight(0)
 
     def build_font(self, text_h):
         if os.name == 'nt':
