e0cac83aeb723d23585fa3a71b455e531319b0d3
Gagan Sopori <gagan.sopori@hotmail.com>
2026-01-28 22:05:21 -0600

    Create base.py

---
+++ states/base.py
@@ -0,0 +1,16 @@
+
+class State:
+    def __init__(self, context):
+        self.context = context
+
+    def enter(self):
+        pass
+
+    def exit(self):
+        pass
+
+    def update(self):
+        pass
+
+    def handle_button(self, button_name):
+        pass
